688bf3464163cdde5ea907505140b7a2fe44eb2e
rodolfo-rgb <rodo.aparicio.lopez@gmail.com>
2025-03-05 02:17:54 -0600

    completed funtions in Lexer.java and added singleChar mapping in TokenType

--- src/main/java/com/github/irmindev/model/TokenType.java
+++ src/main/java/com/github/irmindev/model/TokenType.java
@@ -1,30 +1,50 @@
 package com.github.irmindev.model;
 
 public enum TokenType {
-    // Reserved words
-    ELSE, FALSE, FOR, FUN, IF, NULL, PRINT, RETURN, TRUE, VAR, WHILE,
+  // Reserved words
+  ELSE, FALSE, FOR, FUN, IF, NULL, PRINT, RETURN, TRUE, VAR, WHILE,
 
-    // Operators (Arithmetic)
-    PLUS, MINUS, STAR, SLASH,
+  // Operators (Arithmetic)
+  PLUS, MINUS, STAR, SLASH,
 
-    // Operators (Relational)
-    LESS, GRATER, LESS_EQUAL, GRATER_EQUAL, EQUAL_EQUAL, BANG_EQUAL,
+  // Operators (Relational)
+  LESS, GRATER, LESS_EQUAL, GRATER_EQUAL, EQUAL_EQUAL, BANG_EQUAL,
 
-    // Operators (Logical)
-    AND, OR,
+  // Operators (Logical)
+  AND, OR,
 
-    // Operators (Assignment)
-    EQUAL,
+  // Operators (Assignment)
+  EQUAL,
 
-    // Operators (Negation)
-    BANG,
+  // Operators (Negation)
+  BANG,
 
-    // Punctuation
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, SEMICOLON,
+  // Punctuation
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, SEMICOLON,
 
-    // Data types
-    IDENTIFIER, STRING, NUMBER,
+  // Data types
+  IDENTIFIER, STRING, NUMBER,
 
-    // End of file
-    EOF
+  // End of file
+  EOF;
+
+  public static TokenType fromChar(char c) {
+    return switch (c) {
+      case '+' -> PLUS;
+      case '-' -> MINUS;
+      case '*' -> STAR;
+      case '/' -> SLASH;
+      case '<' -> LESS;
+      case '>' -> GRATER;
+      case '=' -> EQUAL;
+      case '!' -> BANG;
+      case '(' -> LEFT_PAREN;
+      case ')' -> RIGHT_PAREN;
+      case '{' -> LEFT_BRACE;
+      case '}' -> RIGHT_BRACE;
+      case ',' -> COMMA;
+      case ';' -> SEMICOLON;
+      default -> throw new IllegalArgumentException("Unexpected character: " + c);
+    };
+  }
 }
