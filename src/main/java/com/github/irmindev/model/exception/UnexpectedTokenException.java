50bed0957200a5317e44a4d33d616d79281bbfe0
IrminDev <ihernandezj1902@alumno.ipn.mx>
2025-04-01 22:31:17 +0000

    Exception updated

--- src/main/java/com/github/irmindev/model/exception/UnexpectedTokenException.java
+++ src/main/java/com/github/irmindev/model/exception/UnexpectedTokenException.java
@@ -3,14 +3,7 @@ package com.github.irmindev.model.exception;
 import com.github.irmindev.model.TokenType;
 
 public class UnexpectedTokenException extends RuntimeException {
-    private TokenType expected;
-    private TokenType actual;
-    private int line;
-
     public UnexpectedTokenException(TokenType expected, TokenType actual, int line) {
         super("Unexpected token: expected " + expected + ", but got " + actual + " at line " + line);
-        this.expected = expected;
-        this.actual = actual;
-        this.line = line;
     }
 }
