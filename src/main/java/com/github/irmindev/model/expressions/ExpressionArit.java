4af80c4aa58b6054c35c4d031015a2f8b35f795c
IrminDev <ihernandezj1902@alumno.ipn.mx>
2025-05-27 15:59:54 -0600

    Fixes

--- src/main/java/com/github/irmindev/model/expressions/ExpressionArit.java
+++ src/main/java/com/github/irmindev/model/expressions/ExpressionArit.java
@@ -2,14 +2,25 @@ package com.github.irmindev.model.expressions;
 import com.github.irmindev.model.Token;
 
 public class ExpressionArit extends Expression{
-    final Expression left;
-    final Token operator;
-    final Expression right;
+    private final Expression left;
+    private final Token operator;
+    private final Expression right;
 
     public ExpressionArit(Expression left, Token operator, Expression right) {
         this.left = left;
         this.operator = operator;
         this.right = right;
     }
+    
+    public Expression getLeft() {
+        return left;
+    }
 
+    public Token getOperator() {
+        return operator;
+    }
+
+    public Expression getRight() {
+        return right;
+    }
 }
--- src/main/java/com/github/irmindev/model/expressions/ExpressionAssign.java
+++ src/main/java/com/github/irmindev/model/expressions/ExpressionAssign.java
@@ -2,11 +2,19 @@ package com.github.irmindev.model.expressions;
 import com.github.irmindev.model.Token;
 
 public class ExpressionAssign extends Expression{
-    final Token name;
-    final Expression value;
+    private final Token name;
+    private final Expression value;
 
     public ExpressionAssign(Token name, Expression value) {
         this.name = name;
         this.value = value;
+    }
+
+    public Token getName() {
+        return name;
+    }
+
+    public Expression getValue() {
+        return value;
     }
 }
